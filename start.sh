5273f58334953e36ab1f18205e2021958abad2ba
nouchka <github@katagena.com>
2016-05-06 22:45:40 +0200

    Registry 2.4.1 change

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ htpasswd -Bbc /htpasswd $REGISTRY_USER $REGISTRY_PASSWORD
 
 export REGISTRY_AUTH_HTPASSWD_PATH=/htpasswd
 
-/bin/registry /etc/docker/registry/config.yml
+/bin/registry serve /etc/docker/registry/config.yml
