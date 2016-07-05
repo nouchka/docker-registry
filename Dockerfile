082ac176c768310af002a097bdb2026beea10f02
nouchka <github@katagena.com>
2016-07-05 13:13:29 +0200

    auto generate self signed cert if missing

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
-FROM registry:2.4.0
+FROM registry:2.4.1
 MAINTAINER Jean-Avit Promis "docker@katagena.com"
 
 COPY start.sh /start.sh
 RUN chmod +x /start.sh
 
 ENV REGISTRY_PASSWORD registry-password
+
+VOLUME ["/certs", "/var/lib/registry"]
 
 ENTRYPOINT ["/start.sh"]
--- start.sh
+++ start.sh
@@ -4,4 +4,8 @@ htpasswd -Bbc /htpasswd $REGISTRY_USER $REGISTRY_PASSWORD
 
 export REGISTRY_AUTH_HTPASSWD_PATH=/htpasswd
 
+if [ ! -f "$REGISTRY_HTTP_TLS_CERTIFICATE" ]; then
+	openssl req -newkey rsa:4096 -nodes -sha256 -keyout $REGISTRY_HTTP_TLS_KEY -x509 -days 365 -out $REGISTRY_HTTP_TLS_CERTIFICATE -subj "$REGISTRY_HTTP_TLS_SUBJ"
+fi
+
 /bin/registry serve /etc/docker/registry/config.yml
